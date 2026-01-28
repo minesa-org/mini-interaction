8378187a35490d2679ed954433f3ed66a27d5594
İbrahim <neodevils_contact@icloud.com>
2026-01-28 20:01:01 +0300

    Bug fixes and improvements

--- test_type.ts
+++
@@ -1,4 +0,0 @@
-import { ComponentType } from "discord-api-types/v10";
-console.log("Container:", ComponentType.Container);
-console.log("Section:", ComponentType.Section);
-console.log("ActionRow:", ComponentType.ActionRow);
